db82c54da81b569ac829bb6ed9990954c3350fe1
Atryee Dhar <atryeed@trainee.nrifintech.com>
2025-02-04 16:48:00 +0530

    added product form

--- src/components/AddProduct.tsx
+++ src/components/AddProduct.tsx
@@ -1,54 +1,88 @@
-import React, { useState } from 'react'
+import React, { ReactEventHandler, useState } from 'react'
+
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    quantity?: number;
+    stockquantity: number;
+    desc: string;
+    brand: string;
+    category: string;
+    // releaseDate: Date;
+    // available: boolean;
+  }
 
 function AddProduct() {
-    const [product, setProduct] = useState()
-    const addProduct = (formData) => {
+    const [product, setProduct] = useState<Product>({
+        id: 0,
+        name: '',
+        price: 0,
+        quantity: 0,
+        stockquantity: 0,
+        desc: '',
+        brand: '',
+        category: '',
+
+    })
+    const addToProducts = (formData: FormData) => {
+        console.log('add product called...')
         const name = formData.get('name')
         const brand = formData.get('brand')
         const quantity = formData.get('stockquantity')
         const price = formData.get('price')
         const category = formData.get('category')
         const description = formData.get('desc')
+        //console.log('FORMDATA: '+name+' '+brand+' '+quantity+' '+price+' '+category+' '+description)
+    }
+
+    const handleChange = (event : React.ChangeEvent<HTMLInputElement>) => {
+        setProduct({...product,[event.target.name] : event.target.value});
+    }
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(product)
     }
   return (
     <>
     <div>AddProduct</div>
-    <form action={addProduct(FormData)} className='flex-col'>
+    <form action='#' onSubmit={(e)=>handleSubmit(e)} className='flex-col'>
         <div className='p-2 my-2'>
         <label className='mx-2'>
             Product name:
         </label>
-        <input name='name' type="text" className='border-2 border-gray-300'/>
+        <input name='name' type="text" className='border-2 border-gray-300' onChange={(e)=>handleChange(e)}/>
         </div>
         <div className='p-2 my-2'>
         <label className='mx-2'>
             Brand:
         </label>
-        <input name='brand' type="text" className='border-2 border-gray-300'/>
+        <input name='brand' type="text" className='border-2 border-gray-300' onChange={(e)=>handleChange(e)}/>
         </div>
         <div className='p-2 my-2'>
         <label className='mx-2'>
             Quantity:
         </label>
-        <input name='stockquantity' type='number' className='border-2 border-gray-300'/>
+        <input name='stockquantity' type='number' className='border-2 border-gray-300' onChange={(e)=>handleChange(e)}/>
         </div>
         <div className='p-2 my-2'>
         <label className='mx-2'>
             Price:
         </label>
-        <input name='price' type="number" className='border-2 border-gray-300'/>
+        <input name='price' type="number" className='border-2 border-gray-300' onChange={(e)=>handleChange(e)}/>
         </div>
         <div className='p-2 my-2'>
         <label className='mx-2'>
             Category
         </label>
-        <input name='category' type="text" className='border-2 border-gray-300'/>
+        <input name='category' type="text" className='border-2 border-gray-300' onChange={(e)=>handleChange(e)}/>
         </div>
         <div className='p-2 my-2'>
         <label className='mx-2'>
             Description:
         </label>
-        <input name='desc' type="text" className='border-2 border-gray-300'/>
+        <input name='desc' type="text" className='border-2 border-gray-300' onChange={(e)=>handleChange(e)}/>
         </div>
         <button type='submit' className='py-1 px-2 bg-green-600 text-white'>Submit</button>
     </form>
