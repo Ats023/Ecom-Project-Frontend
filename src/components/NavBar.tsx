712d71038de4e2b24cd4fe6f4b4b6e6f131ca150
Atryee Dhar <atryeed@trainee.nrifintech.com>
2025-02-06 17:04:09 +0530

    updated search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,6 +8,9 @@ const NavBar = () => {
                 <Link to='/' className="m-auto py-2 px-2">Home</Link>
                 <Link to='/about' className="m-auto py-2 px-2">About</Link>
             </div>      
+            <div>
+                
+            </div>
             <div className="flex justify-around">
                 <Link to='/products/add' className="m-auto py-1 px-2">Add Product</Link>
                 <Link to='/cart' className="m-auto py-1 px-2">Go to cart</Link>
